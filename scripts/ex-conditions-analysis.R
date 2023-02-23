2f93060c2f6ffccdc6c25de7fcb1ca75ff89bec4
Hannah Kweon <ykweon@nelsonnygaard.com>
2023-02-23 12:47:14 -0500

    add ridership

--- scripts/ex-conditions-analysis.R
+++ scripts/ex-conditions-analysis.R
@@ -29,6 +29,8 @@ base_gdb <- "G:\\Current\\WOODBURN_Transit_Plan_2021.0328\\Analysis\\GDB\\Woodbu
 census_layer = "Blocks_Wdb_2020CensusPop_2019Jobs"
 acs_wdb_layer = "BlockGroups_ACS2020_WdbArea"
 acs_layer = "BlockGroups_ACS2020"
+activity_center_layer="Community_Development_DBO_School_Locations"
+ridership_layer = 'Remix_Stops_Wdb_Ridership'
 
 # database layer list
 fc_list <- ogrListLayers(base_gdb)
@@ -45,6 +47,12 @@ bg_acs_wdb_data <- sf::st_read(base_gdb,
 
 bg_acs_data <- sf::st_read(base_gdb, 
                            layer = acs_layer)
+
+ac_data <- sf::st_read(base_gdb, 
+                       layer = activity_center_layer)
+
+ridership_data <- sf::st_read(base_gdb, 
+                              layer = ridership_layer)
 
 ex_gtfs <- read_gtfs("input/gtfs/Woodburn_GTFS_Fall22_RtsStops.zip")
 
@@ -83,6 +91,9 @@ stop_iso <- stops %>%
 
 # union isochrone
 stop_iso_union = stop_iso %>% st_union()
+
+# save unioned isochrone
+st_write(stop_iso_union, "output/woodburn_walkshed_quarter_mi.shp")
 
 # Population & Job =============================================================
 ## calculate population and job at block level which is covered by walkshed
@@ -144,7 +155,57 @@ demo_stat <- stop_iso_union_demo_inters %>%
 
 write.xlsx(demo_stat, "output/demographic_walkshed_quarter_mi_bg.xlsx")
 
+# Ridership  ===================================================================
+## calculate ridership within walkshed
+
+ridership_data <- ridership_data %>% 
+  st_transform(coord_global)
+
+stop_iso_union_ridership_inters <- st_intersection(ridership_data, stop_iso_union)
 
 
+ridership_stat <- data.frame(
+  "FR_ons_sum_wdb" = NA,
+  "ER_ons_sum_wdb" = NA,
+  "Total_ons_sum_wdb" = NA,
+  "FR_ons_sum_walkshed" = NA,
+  "ER_ons_sum_walkshed" = NA,
+  "Total_ons_sum_walkshed" = NA
+)
+
+ridership_stat$FR_ons_sum_wdb = sum(ridership_data$FR_Ons, na.rm = TRUE)
+ridership_stat$ER_ons_sum_wdb = sum(ridership_data$ER_Ons, na.rm = TRUE)
+ridership_stat$Total_ons_sum_wdb = sum(ridership_data$Total_Ons, na.rm = TRUE)
+ridership_stat$FR_ons_sum_walkshed = sum(stop_iso_union_ridership_inters$FR_Ons, na.rm = TRUE)
+ridership_stat$ER_ons_sum_walkshed = sum(stop_iso_union_ridership_inters$ER_Ons, na.rm = TRUE)
+ridership_stat$Total_ons_sum_walkshed = sum(stop_iso_union_ridership_inters$Total_Ons, na.rm = TRUE)
+
+ridership_stat <- ridership_stat %>% 
+  mutate(FR_ons_perc = FR_ons_sum_walkshed/FR_ons_sum_wdb,
+         ER_ons_perc = ER_ons_sum_walkshed/ER_ons_sum_wdb,
+         ER_ons_perc = ER_ons_sum_walkshed/ER_ons_sum_wdb)
+
+# Activity Center  =============================================================
+## calculate Activity Center within walkshed
+
+ac_data <- ac_data %>% 
+  st_transform(coord_global) %>% 
+  filter(Include_Eval == 1)
+
+stop_iso_union_ac_inters <- st_intersection(ac_data, stop_iso_union)
 
 
+ac_stat <- data.frame(
+  activity_center_num_wdb = NA,
+  activity_center_num_walkshed = NA
+)
+
+ac_stat$activity_center_num_wdb = nrow(ac_data)
+ac_stat$activity_center_num_walkshed = nrow(stop_iso_union_ac_inters)
+
+ac_stat <- ac_stat %>% 
+  mutate(activity_center_perc = activity_center_num_walkshed/activity_center_num_wdb)
+
+ridership_ac <- cbind(ridership_stat, ac_stat)
+
+write.xlsx(ridership_ac, "output/ridership_activity_center_walkshed_quarter_mi_bg.xlsx")
